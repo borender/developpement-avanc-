c82eb5a019c8a9310e0bce4fd5e28ede0778f0a0
borender <53793070+borender@users.noreply.github.com>
2022-05-06 09:53:05 +0200

    project-quentin

---
+++ developpement-avance/QuentinR-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuentinR_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
