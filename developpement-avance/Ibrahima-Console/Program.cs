78a12dfd4ca076e519cd70118610a03b9bfb95ff
borender <53793070+borender@users.noreply.github.com>
2022-05-06 10:34:08 +0200

    add ibra

---
+++ developpement-avance/Ibrahima-Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ibrahima_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[9];
+            tab[0] = 1;
+            tab[1] = 3;
+            tab[2] = 2;
+            tab[3] = 5;
+            tab[4] = 7;
+            tab[5] = 8;
+            tab[6] = 9;
+            tab[7] = 4;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                
+                Console.WriteLine("Tab : " + tab[i]);
+            }
+           
+        }
+    }
+}
