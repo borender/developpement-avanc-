a077116af0042a74466adde295bf8f4f950cff7c
borender <53793070+borender@users.noreply.github.com>
2022-07-05 14:36:16 +0200

    front

--- developpement-avance/Ibrahima-Console/Program.cs
+++ developpement-avance/Ibrahima-Console/Program.cs
@@ -15,7 +15,7 @@ namespace Ibrahima_Console
             tab[5] = 8;
             tab[6] = 9;
             tab[7] = 4;
-            int a = 0, b = 0;
+            int a = 0;
             for (int i = 0; i < tab.Length; i++)
             {
                 for (int j = 1; j < tab.Length; j++)
--- developpement-avance/QuentinR-console/Program.cs
+++ developpement-avance/QuentinR-console/Program.cs
@@ -15,21 +15,25 @@ namespace QuentinR_console
             tab[5] = 8;
             tab[6] = 9;
             tab[7] = 4;
-            int a = 0, b = 0;
+            int a = 0;
+            int b = 0;
+            int c = 0;
             for (int i = 0; i < tab.Length; i++)
             {
-                for (int j = 1; j < tab.Length; j++)
+
+                for (int j = 0; j < tab.Length; j++)
                 {
-
-                    if (tab[i] == tab[j] - 1)
+                    b = tab[j] - 1;
+                    
+                    if (tab[i] == (tab[j] - 1))
                     {
-                        a = tab[i];
-                        tab[i] = tab[j];
-                        tab[j] = a;
-
+                        a = tab[j];
+                        Console.WriteLine(a);
+                        //Console.WriteLine("tab i "+tab[i]);
+                        //Console.WriteLine("tab j "+b);
+                        continue;
                     }
-                    Console.WriteLine(tab[i]);
-                    i++;
+                    
                 }
 
             }
